df8436c0822671f7d04006cc903423413081f524
Ralph Yozzo <ralph@brooklynmarathon.com>
2014-12-05 17:09:55 -0500

    add formatting

---
+++ format.sh
@@ -0,0 +1,2 @@
+#!
+./format/nyc_comptroller_claims.py < output/nyc_comptroller_claims.csv > output/formatted_nyc_comptroller_claims.csv
---
+++ format/nyc_comptroller_claims.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+
+import sys
+import csv
+import re
+from datetime import datetime
+
+
+csvwriter = csv.writer(sys.stdout, delimiter=',', quotechar='"')
+reader = csv.DictReader( sys.stdin )
+
+
+for line in reader:
+    row=[]
+    for key in line:
+        #print( key )
+        if '_date' in key:
+            try:
+                date = datetime.strptime(line[key],'%m/%d/%Y')
+                #row.append(datetime.strftime(date,'%Y-%m-%dT%H:%M:%S-05:00'))
+                row += [ datetime.strftime(date,'%Y-%m-%dT%H:%M:%S-05:00') ]
+            except Exception,e:
+                #print 'issue1:',e, key, line[key]
+                row += [ '0000-01-01T00:00']
+                pass
+        elif '_amount' in key:
+            try:
+                row += [ float(line[key].replace(',','').replace('$','')) ]
+            except Exception,e:
+                #print 'issue1:',e, key, line[key]
+                #row += [ 'issue: %s' % line[key]]
+                row += [ '' ]
+                pass
+        else:
+                row += [ line[key] ]
+    csvwriter.writerow(row)
+                  
+# date = datetime.strptime(line[key],'%m/%d/%Y')
+# row.append(datetime.strftime(date,'%Y-%m-%dT%H:%M:%S-05:00'))
+# except Exception,e:
+# #print 'issue1:',e, key, line[key]
+# row.append('')
