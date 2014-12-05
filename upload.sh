21c4513204baf49a985c6db6b66f78d8250d3081
Ralph Yozzo <ralph@brooklynmarathon.com>
2014-12-05 17:24:36 -0500

    add proper format

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@
 # bq --project proj-wikimeasure --nosync load --max_bad_records 1 --skip_leading_rows 1 analysis.nyc_comptroller_claims output/nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
 
 # bq --project proj-wikimeasure --nosync load --max_bad_records 1 --skip_leading_rows 1 analysis.nyc_comptroller_claims output/formatted_nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
-bq --project proj-wikimeasure --nosync load --max_bad_records 1 --skip_leading_rows 0 analysis.nyc_comptroller_claims output/formatted_nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
+bq --project proj-wikimeasure --nosync load --max_bad_records 1 --replace --skip_leading_rows 0 analysis.nyc_comptroller_claims output/formatted_nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
