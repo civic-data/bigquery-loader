fceef42b7808bb8d65e5063054c64954d0d1a6c0
Ralph Yozzo <fedex1@gmail.com>
2014-12-19 22:59:14 -0500

    add ticket database

--- upload.sh
+++ upload.sh
@@ -5,4 +5,6 @@
 # bq --project proj-wikimeasure --nosync load --max_bad_records 1 --skip_leading_rows 1 analysis.nyc_comptroller_claims output/nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
 
 # bq --project proj-wikimeasure --nosync load --max_bad_records 1 --skip_leading_rows 1 analysis.nyc_comptroller_claims output/formatted_nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
-bq --project proj-wikimeasure --nosync load --max_bad_records 1 --replace --skip_leading_rows 0 analysis.nyc_comptroller_claims output/formatted_nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
+# bq --project proj-wikimeasure --nosync load --max_bad_records 1 --replace --skip_leading_rows 0 analysis.nyc_comptroller_claims output/formatted_nyc_comptroller_claims.csv schemas/nyc_comptroller_claims.json
+
+bq --project personal-real-estate load --max_bad_records 1 --replace --skip_leading_rows 0 nys.traffic_tickets ~/Downloads/Traffic_Tickets_Issued__Beginning_2008.csv schemas/nys_traffic_tickets_2008_2014.json
